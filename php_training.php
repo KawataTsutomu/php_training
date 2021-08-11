f9ce18348269f0a53877ce58c6d9a10813f9817b
kawatatsutomu <tomtom.net1@gmail.com>
2021-08-11 09:10:25 +0900

    foreach 連想配列

--- php_training.php
+++ php_training.php
@@ -226,14 +226,19 @@ if (array_key_exists("age", $data2)) {
 echo PHP_EOL;
 ?>
 
-print("#####q16#####".PHP_EOL);
+<?php
+print("#####q16#####" . PHP_EOL);
+// 次の配列の各要素について，「私の名前は〜です。年齢は〜歳です。」と表示して下さい。
 $users = [
-["name" => "satou", "age" => 22],
-["name" => "yamada", "age" => 12],
-["name" => "takahashi", "age" => 32],
-["name" => "nakamura", "age" => 41]
+    ["name" => "satou", "age" => 22],
+    ["name" => "yamada", "age" => 12],
+    ["name" => "takahashi", "age" => 32],
+    ["name" => "nakamura", "age" => 41]
 ];
 
 # 以下に回答を記載
-
+foreach ($users as $user) {
+    echo "私の名前は" . $user["name"] . "です。年齢は" . $user["age"] . "歳です。" . PHP_EOL;
+}
 echo PHP_EOL;
+?>
