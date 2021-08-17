4146172d752dcafc15d569f8af5af2b86a9f0d5b
kawatatsutomu <tomtom.net1@gmail.com>
2021-08-17 22:19:24 +0900

    配列 追加

--- php_training.php
+++ php_training.php
@@ -1,343 +1,132 @@
 <?php
-
 print("#####q1#####" . PHP_EOL);
-// 次の配列の最後に "斎藤" を追加し，出力して下さい。
+// 次の配列の最後に "斎藤" を追加し、出力して下さい。
 $names = ["田中", "佐藤", "佐々木", "高橋"];
 
-# 以下に回答を記載
+// 以下に回答を記載
 $names[] = "斎藤";
 print_r($names);
 echo PHP_EOL;
-
 ?>
 
-<?php
-print("#####q2#####" . PHP_EOL);
-// 次の二つの配列を合体させた新しい配列 $array を作成し，出力して下さい。
+print("#####q2#####".PHP_EOL);
 $array1 = ["dog", "cat", "fish"];
 $array2 = ["bird", "bat", "tiger"];
 
-# 以下に回答を記載
-$array = array_merge($array1, $array2);
+// 以下に回答を記載
 
-print_r($array);
 echo PHP_EOL;
-?>
 
-<?php
-print("#####q3#####" . PHP_EOL);
-// 次の配列の中に 3 がいくつあるかを出力するコードを書き，出力して下さい。
+print("#####q3#####".PHP_EOL);
 $numbers = [1, 5, 8, 10, 2, 3, 2, 3, 1, 4, 5, 9];
 
-# 以下に回答を記載
-$count = 0;
-foreach ($numbers as $number) {
-    if ($number === 3) {
-        $count++;
-    }
-}
+// 以下に回答を記載
 
-print_r($count . "個" . PHP_EOL);
 echo PHP_EOL;
-?>
 
-<?php
-print("#####q4#####" . PHP_EOL);
-// 次の配列から null の要素を削除し，出力して下さい。（新しい配列を作成せずに実現して下さい）
+print("#####q4#####".PHP_EOL);
 $sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
 
-# 以下に回答を記載
-$sports = array_diff($sports, [null]);
-print_r($sports);
-echo PHP_EOL;
-?>
+// 以下に回答を記載
 
-<?php
-print("#####q5#####" . PHP_EOL);
-// 配列が空であれば true，1つ以上の要素があれば false を出力するコードを書いて下さい。
+echo PHP_EOL;
+
+print("#####q5#####".PHP_EOL);
 $array1 = [];
 $array2 = [1, 5, 8, 10];
 
-# 以下に回答を記載
-var_export(empty($array1));
+// 以下に回答を記載
+
 echo PHP_EOL;
 
-var_export(empty($array2));
-echo PHP_EOL;
-?>
-
-<?php
-print("#####q6#####" . PHP_EOL);
-// 次の配列から，期待された結果の配列 $numbers2 を作成し，出力して下さい。
+print("#####q6#####".PHP_EOL);
 $numbers1 = [1, 2, 3, 4, 5];
 
-# 以下に回答を記載
-$numbers2 = [];
-foreach ($numbers1 as $number) {
-    $numbers2[] = $number * 10;
-}
+// 以下に回答を記載
 
-print_r($numbers2);
 echo PHP_EOL;
-?>
 
-<?php
-print("#####q7#####" . PHP_EOL);
-// 次の配列の要素を 文字列 から 数字 に変換し，出力して下さい。（新しい配列を作成せずに実現して下さい）
+print("#####q7#####".PHP_EOL);
 $array = ["1", "2", "3", "4", "5"];
 
-# 以下に回答を記載
-$array = array_map('intval', $array);
-# 以下は変更しないで下さい
+// 以下に回答を記載
+
+// 以下は変更しないで下さい
 var_dump($array);
 
 echo PHP_EOL;
-?>
 
-<?php
-print("#####q8#####" . PHP_EOL);
+print("#####q8#####".PHP_EOL);
 $programming_languages = ["php", "ruby", "python", "javascript"];
 
-# 以下に回答を記載
-$programming_languages = array_map('ucfirst', $programming_languages);
-$upper_case_programming_languages = array_map('strtoupper', $programming_languages);
+// 以下に回答を記載
 
-# 以下は変更しないで下さい
+// 以下は変更しないで下さい
 print_r($programming_languages);
 echo PHP_EOL;
 print_r($upper_case_programming_languages);
 
 echo PHP_EOL;
-?>
 
-<?php
-print("#####q9#####" . PHP_EOL);
-// 次の配列を用いて，期待通りの出力結果になるようにコードを書いて下さい。
+print("#####q9#####".PHP_EOL);
 $names = ["田中", "佐藤", "佐々木", "高橋"];
 
-# 以下に回答を記載
-$names2 = [];
-foreach ($names as $key => $name) {
-    $number = $key + 1;
-    $name = "会員No." . $number . " " . $name;
-    $names2[] = $name;
-}
+// 以下に回答を記載
 
-print_r($names2);
 echo PHP_EOL;
-?>
 
-<?php
-print("#####q10#####" . PHP_EOL);
-// 次の配列の各要素について， うに という文字列が含まれていれば「好物です」と表示し，そうでなければ「まぁまぁ好きです」と出力するコードを書いて下さい。
+print("#####q10#####".PHP_EOL);
 $foods = ["いか", "たこ", "うに", "しゃけ", "うにぎり", "うに軍艦", "うに丼", "高級なうに"];
 
-# 以下に回答を記載
-foreach ($foods as $food) {
-    if (strpos($food, 'うに') !== false) {
-        echo "好物です" . PHP_EOL;
-    } else {
-        echo "まぁまぁ好きです" . PHP_EOL;
-    }
-}
-echo PHP_EOL;
-?>
+// 以下に回答を記載
 
-<?php
-print("#####q11#####" . PHP_EOL);
-// 次の配列を用いて，期待する出力結果になるようにコードを書いて下さい。
+echo PHP_EOL;
+
+print("#####q11#####".PHP_EOL);
 $sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]];
 
-# 以下に回答を記載
-$sports2 = [];
-foreach ($sports as $sport) {
-    if (is_array($sport)) {
-        $sports2 = array_merge($sports2, $sport);
-    } else {
-        array_push($sports2, $sport);
-    }
-}
-$sports2 = array_unique($sports2);
-$sports2 = array_values($sports2);
-
-print_r("ユーザーの趣味一覧" . PHP_EOL);
-foreach ($sports2 as $key => $sport) {
-    $number = $key + 1;
-    $number = "No" . $number;
-    $sports2 = $number . " " . $sport . PHP_EOL;
-    print_r($sports2);
-}
+// 以下に回答を記載
 
 echo PHP_EOL;
-?>
 
-<?php
-print("#####q12#####" . PHP_EOL);
-// 次の連想配列から name の値を出力して下さい。
+print("#####q12#####".PHP_EOL);
 $data = ["user" => ["name" => "satou", "age" => 33]];
 
-# 以下に回答を記載
-print_r($data["user"]["name"]);
-echo PHP_EOL;
-?>
+// 以下に回答を記載
 
-<?php
-print("#####q13#####" . PHP_EOL);
-//  次の $user_data に，$update_data の内容を反映させ，$user_data の内容を書き換え，出力して下さい。
+echo PHP_EOL;
+
+print("#####q13#####".PHP_EOL);
 $user_data = ["name" => "神里", "age" => 31, "address" => "埼玉"];
 $update_data = ["age" => 32, "address" => "沖縄"];
 
-# 以下に回答を記載
-$user_data = array_merge($user_data, $update_data);
-print_r($user_data);
-echo PHP_EOL;
-?>
+// 以下に回答を記載
 
-<?php
-print("#####q14#####" . PHP_EOL);
-// 次の連想配列から全てのキー（添字）を取り出して、キーが数字である通常の配列（添字配列）を作成し，出力して下さい。
+echo PHP_EOL;
+
+print("#####q14#####".PHP_EOL);
 $data = ["name" => "satou", "age" => 33, "address" => "saitama", "hobby" => "soccer", "email" => "hoge@fuga.com"];
 
-# 以下に回答を記載
-$data = array_values($data);
-print_r($data);
-echo PHP_EOL;
-?>
+// 以下に回答を記載
 
-<?php
-print("#####q15#####" . PHP_EOL);
-// age というキーが含まれている場合は OK ，含まれていない場合は NG という文字列が出力されるコードを書いて下さい。
+echo PHP_EOL;
+
+print("#####q15#####".PHP_EOL);
 $data1 = ["name" => "saitou", "hobby" => "soccer", "age" => 33, "role" => "admin"];
 $data2 = ["name" => "yamada", "hobby" => "baseball", "role" => "normal"];
 
-# 以下に回答を記載
-if (array_key_exists("age", $data1)) {
-    echo "OK" . PHP_EOL;
-} else {
-    echo "NG" . PHP_EOL;
-}
-
-if (array_key_exists("age", $data2)) {
-    echo "OK" . PHP_EOL;
-} else {
-    echo "NG" . PHP_EOL;
-}
+// 以下に回答を記載
 
 echo PHP_EOL;
-?>
 
-<?php
-print("#####q16#####" . PHP_EOL);
-// 次の配列の各要素について，「私の名前は〜です。年齢は〜歳です。」と表示して下さい。
+print("#####q16#####".PHP_EOL);
 $users = [
-    ["name" => "satou", "age" => 22],
-    ["name" => "yamada", "age" => 12],
-    ["name" => "takahashi", "age" => 32],
-    ["name" => "nakamura", "age" => 41]
+["name" => "satou", "age" => 22],
+["name" => "yamada", "age" => 12],
+["name" => "takahashi", "age" => 32],
+["name" => "nakamura", "age" => 41]
 ];
 
-# 以下に回答を記載
-foreach ($users as $user) {
-    echo "私の名前は" . $user["name"] . "です。年齢は" . $user["age"] . "歳です。" . PHP_EOL;
-}
-echo PHP_EOL;
-?>
-
-<?php
-print("#####q17#####" . PHP_EOL);
-// 次の Userクラス 内にinfoメソッドを追加し、期待する出力結果になるようにして下さい。
-class User
-{
-
-    // コードを追加
-    //プロパティ(変数)を宣言する
-    private $name;
-    private $age;
-    private $gender;
-
-    //コンストラクタメソッドを宣言
-    function __construct($user_name, $user_age, $user_gender)
-    {
-        $this->name = $user_name;
-        $this->age = $user_age;
-        $this->gender = $user_gender;
-    }
-
-    //出力
-    function info()
-    {
-        print("名前:" . $this->name . PHP_EOL);
-        print("年齢:" . $this->age . PHP_EOL);
-        print("性別:" . $this->gender . PHP_EOL);
-    }
-}
-
-$user1 = new User("神里", 32, "男");
-$user2 = new User("あじー", 32, "男");
-
-$user1->info();
-print("-------------" . PHP_EOL);
-$user2->info();
-
-echo PHP_EOL;
-?>
-
-print("#####q18#####".PHP_EOL);
-
-// コードを追加
-
-$man1 = new Man("あじー", 32);
-$man2 = new Man("ゆたぼん", 10);
-
-$man1->introduce();
-$man2->introduce();
-
-echo PHP_EOL;
-
-print("#####q19#####".PHP_EOL);
-class Item
-{
-// 以下を修正して下さい
-
-protected $name;
-
-function __construct($book_name){
-$this->name = $book_name;
-}
-}
-// 以下は変更しないで下さい
-
-$book = new Item("ゼロ秒思考");
-print($book->name.PHP_EOL);
-
-echo PHP_EOL;
-
-print("#####q20#####".PHP_EOL);
-class Human
-{
-
-// コードを追加
-
-}
-
-class Zoo
-{
-
-// コードを追加
-
-}
-
-$zoo = new Zoo("旭山動物園",["infant" => 0, "children" => 400, "adult" => 800, "senior" => 500]);
-
-$human1 = new Human("たま", 3);
-$human2 = new Human("ゆたぼん", 10);
-$human3 = new Human("あじー", 32);
-$human4 = new Human("ぎん", 108);
-
-$humans = [$human1, $human2, $human3, $human4];
-
-foreach ($humans as $human) {
-$zoo->info_entry_fee($human);
-}
+// 以下に回答を記載
 
 echo PHP_EOL;
