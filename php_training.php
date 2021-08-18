93343cba04b490ce45bf5b60ee6a95c91560f6aa
kawatatsutomu <tomtom.net1@gmail.com>
2021-08-18 13:05:01 +0900

    配列 書き換え

--- php_training.php
+++ php_training.php
@@ -177,13 +177,17 @@ print_r($data["user"]["name"]);
 echo PHP_EOL;
 ?>
 
-print("#####q13#####".PHP_EOL);
+<?php
+print("#####q13#####" . PHP_EOL);
+// 次の $user_data に、$update_data の内容を反映させ、$user_data の内容を書き換え、出力して下さい。
 $user_data = ["name" => "神里", "age" => 31, "address" => "埼玉"];
 $update_data = ["age" => 32, "address" => "沖縄"];
 
 // 以下に回答を記載
-
+$user_data = array_merge($user_data, $update_data);
+print_r($user_data);
 echo PHP_EOL;
+?>
 
 print("#####q14#####".PHP_EOL);
 $data = ["name" => "satou", "age" => 33, "address" => "saitama", "hobby" => "soccer", "email" => "hoge@fuga.com"];
